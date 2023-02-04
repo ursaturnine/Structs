5ef77c5d2b002d70329bd04ecff4dc3de296e6c3
Tyrah <tyrah96@gmail.com>
2023-02-04 14:06:00 -0800

    embed contactInfo struct into person struct. created person value with this structure

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
-import "fmt"
+// embed struct inside another -contact in person
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
 
 // Define type, person
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 // Create a value of type, person
 func main() {
-	Isaac := person{firstName: "Isaac", lastName: "Clarke"}
-	fmt.Println(Isaac)
+	Isaac := person{
+		firstName: "Isaac",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "Isaacatsomethingdotcom",
+			zipCode: 98112,
+		},
+	}
 }
